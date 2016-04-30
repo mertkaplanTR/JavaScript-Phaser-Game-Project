b99030533df217bd73ca4885422cf854b2c7dff0
mertkaplan_tr <mertkaplan.tr@gmail.com>
2016-05-01 00:59:52 +0300

    v1

---
+++ Screens/ScreenManager.js
@@ -0,0 +1,21 @@
+﻿
+var gameWidth = 800;
+var gameHeight = 450;
+
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'KulturGameDev');
+var mainState={
+    preload: function () {
+
+    },
+    
+    create: function () {
+    },
+
+    update: function () {
+
+    }
+
+}
+
+game.state.add('mainState', mainState);
+game.state.start('mainState');
